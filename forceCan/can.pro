0d8915b22991f40df28f7ab30f36220ae362ce22
fengbaochun <849067797@qq.com>
2022-03-07 15:48:03 +0800

    add

--- forceCan/can.pro
+++ forceCan/can.pro
@@ -37,3 +37,6 @@ win32: LIBS += -L$$PWD/./ -lControlCAN
 qnx: target.path = /tmp/$${TARGET}/bin
 else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
+
+RESOURCES += \
+    pic.qrc
--- forceCan/main.cpp
+++ forceCan/main.cpp
@@ -6,6 +6,9 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.setWindowTitle("压力变送器V1.0");
+    QIcon icon(":/new/prefix1/img/check.pngg");     //:/new/prefix1/img/check.png
+    w.setWindowIcon(icon);
     w.show();
     return a.exec();
 }
