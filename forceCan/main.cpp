1bb9ab46b9b7f1393eee73180e310d6ba4e34955
fengbaochun <849067797@qq.com>
2022-05-10 15:11:41 +0800

    完善 UI

--- forceCan/main.cpp
+++ forceCan/main.cpp
@@ -1,4 +1,4 @@
-#include "mainwindow.h"
+﻿#include "mainwindow.h"
 
 #include <QApplication>
 
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.setWindowTitle("压力变送器V1.0");
+    w.setWindowTitle("压力变送器V1.2");
     QIcon icon(":/img/favicon.ico");
     w.setWindowIcon(icon);
     w.show();
