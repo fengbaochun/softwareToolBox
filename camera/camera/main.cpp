174d74910ff4ff44cda2775d1a7945827bc5848a
fengbaochun <849067797@qq.com>
2022-09-13 22:10:24 +0800

    1、支持高分屏

--- camera/camera/main.cpp
+++ camera/camera/main.cpp
@@ -1,9 +1,14 @@
 #include "mainwindow.h"
-
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);        //开启高分辨率支持
+    QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);           //处理图像模糊问题
+    QGuiApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
+
+    qputenv("QT_SCALE_FACTOR", "0.95");                                 //qt界面缩放倍率
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
